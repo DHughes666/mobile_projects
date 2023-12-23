0258c0a45f9ca2a8dceac9cca0401e013354ed4f
DHughes666 <graziemolto6@gmail.com>
2023-12-23 18:07:10 +0100

    Flexing items ...rating and isOpen

--- Meals2Go/src/features/restaurants/components/restaurant-infoCard-comp.jsx
+++ Meals2Go/src/features/restaurants/components/restaurant-infoCard-comp.jsx
@@ -4,6 +4,9 @@ import { Text } from "react-native";
 import { Card} from 'react-native-paper';
 import { SvgXml } from "react-native-svg";
 
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
 const RestaurantCard = styled(Card)`
     background-color: ${({theme}) => theme.colors.bg.primary};
 `
@@ -12,9 +15,22 @@ const RestaurantCardCover = styled(Card.Cover)`
     padding: ${({theme}) => theme.space[4]};
     background-color: ${({theme}) => theme.colors.bg.secondary};
 `
-
 const CardInfo = styled.View`
     padding: ${({theme}) => theme.space[3]};
+`
+const Rating = styled.View`
+    flex-direction: row;
+    padding-top: ${({theme}) => theme.space[2]};
+    padding-bottom: ${({theme}) => theme.space[2]};
+`
+const Section = styled.View`
+    flex-direction: row;
+    align-items: center
+`
+const SectionEnd = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: flex-end;
 `
 const Address = styled.Text`
     font-family: ${({theme}) => theme.fonts.body};
@@ -39,11 +55,24 @@ export const RestaurantInfo = ({restaurant={}}) => {
         rating = 4, 
         isClosedTemporarily
     } = restaurant;
+
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
+
     return (
         <RestaurantCard elevation={5}>
             <RestaurantCardCover key={name} source={{uri: photos[0]}}/>
             <CardInfo>
                 <Title>{name}</Title>
+                <Section>
+                    <Rating>
+                        {ratingArray.map((rate, index) => (
+                            <SvgXml key={index} xml={star} width={20} height={20}/>
+                        ))}
+                    </Rating>
+                    <SectionEnd>
+                        {isOpenNow && <SvgXml xml={open} width={20} height={20}/>}
+                    </SectionEnd>
+                </Section>
                 <Address>{address}</Address>
             </CardInfo>
         </RestaurantCard>
