e5e01f998d258ac8c139d99952697c789baabd11
DHughes666 <graziemolto6@gmail.com>
2023-12-24 19:55:18 +0100

    Navigation bar ... In progess

--- Meals2Go/App.js
+++ Meals2Go/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
-
+import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {useFonts as useOswald, 
 	Oswald_400Regular} from '@expo-google-fonts/oswald';
 import {useFonts as useLato, 
@@ -9,6 +11,7 @@ import {useFonts as useLato,
 import {theme} from "./src/infrastructure/theme/index"
 
 import RestaurantScreen from "./src/features/restaurants/screens/restaurant-screen";
+import { SafeArea } from "./src/features/restaurants/screens/restaurant-screen-styles";
 
 export default function App() {
 
@@ -24,10 +27,30 @@ export default function App() {
 		return null;
 	}
 
+	const Tab = createBottomTabNavigator();
+
+	const Map = () => {
+		return <SafeArea>
+			<Text>Map</Text>
+		</SafeArea>
+	}
+
+	const Settings = () => {
+		return <SafeArea>
+			<Text>Settings</Text>
+		</SafeArea>
+	}
+
 	return (
 		<>
 			<ThemeProvider theme={theme}>
-				<RestaurantScreen />
+				<NavigationContainer>
+					<Tab.Navigator>
+						<Tab.Screen name="Restaurants" component={RestaurantScreen} />
+						<Tab.Screen name="Map" component={Map} />
+						<Tab.Screen name="Settings" component={Settings} />
+					</Tab.Navigator>
+				</NavigationContainer>
 			</ThemeProvider>
 			<StatusBar style="auto" />
 		</>
