e550392e378685a741fceb4fcdac4e485b313a4a
DHughes666 <graziemolto6@gmail.com>
2023-12-26 10:42:30 +0100

    Data fetching ...Stage 2 - photos ... Successful

--- Meals2Go/src/services/restaurant/restaurant_service.js
+++ Meals2Go/src/services/restaurant/restaurant_service.js
@@ -1,7 +1,7 @@
-import { mocks } from "./mock"
+import { mocks, mockImages } from "./mock"
 import camelize from "camelize";
 
-export const restaurantsRequest = (location="41.878113,-87.629799") => {
+export const restaurantsRequest = (location="43.653225,-79.383186") => {
     return new Promise((resolve, reject) => {
         const mock = mocks[location]
         if(!mock) {
@@ -13,7 +13,9 @@ export const restaurantsRequest = (location="41.878113,-87.629799") => {
 
 export const restaurantsTransform = ({results = []}) => {
     const mappedResults = results.map((restaurant) => {
-        restaurant.photos = [];
+        restaurant.photos = restaurant.photos.map(() => {
+            return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))]
+        })
         return {
             ...restaurant,
             isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARY',
