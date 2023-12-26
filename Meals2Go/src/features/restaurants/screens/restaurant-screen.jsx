f539426e9968102489ccc568b5041baf56f575cb
DHughes666 <graziemolto6@gmail.com>
2023-12-26 15:11:46 +0100

    Data fetching ...Stage 3 ...Activity indicator ... Successful

--- Meals2Go/src/features/restaurants/screens/restaurant-screen.jsx
+++ Meals2Go/src/features/restaurants/screens/restaurant-screen.jsx
@@ -1,16 +1,29 @@
 import { useContext } from "react";
+import { View } from "react-native";
 import { RestaurantContext } from "../../../services/restaurant/restaurant_context";
-import { Searchbar } from "react-native-paper";
+import { Searchbar, ActivityIndicator, MD2Colors } from "react-native-paper";
 import { SafeArea, SearchContainer, 
 	RestaurantList } from "./restaurant-screen-styles";
 import { RestaurantInfo } from "../components/restaurant-infoCard-comp";
 
 const RestaurantScreen = () => {
 	const {isLoading, restaurants, error} = useContext(RestaurantContext)
-	console.log(restaurants);
+	
 	return (
 		<>
 			<SafeArea>
+				{isLoading && (
+					<View style={{
+						position: "absolute", top: "50%", left: "50%",
+					}}>
+						<ActivityIndicator 
+							size={50}
+							style={{marginLeft: -25}}
+							animating={true}
+							color={MD2Colors.blue200}
+						/>
+					</View>
+				)}
 				<SearchContainer>
 					<Searchbar placeholder="Google me"/>
 				</SearchContainer>
