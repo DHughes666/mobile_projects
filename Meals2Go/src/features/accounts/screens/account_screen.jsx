f474b1213bd8b90dfea636f711648371bdf044bf
DHughes666 <graziemolto6@gmail.com>
2024-01-05 19:17:48 +0100

    Account screen ...In progess

--- Meals2Go/src/features/accounts/screens/account_screen.jsx
+++ Meals2Go/src/features/accounts/screens/account_screen.jsx
@@ -1,8 +1,29 @@
-import { AccountBackground } from "../components/account_styles";
+import { AccountBackground, AccountCover, 
+    AccountContainer, AuthButton } from "../components/account_styles";
+import { Spacer } from "../../../components/spacer/spacer-comp";
 
-const AccountScreen = () => {
+const AccountScreen = ({navigation}) => {
     return (
-        <AccountBackground />
+        <AccountBackground>
+            <AccountCover />
+            <AccountContainer>
+                <AuthButton
+                    icon="lock-open-outline"
+                    mode="contained"
+                    onPress={navigation.navigate("login")}
+                >
+                    Login
+                </AuthButton>
+                <Spacer size="large"/>
+                <AuthButton
+                    icon="lock-open-outline"
+                    mode="contained"
+                    onPress={navigation.navigate("register")}
+                >
+                    Register
+                </AuthButton>
+            </AccountContainer>
+        </AccountBackground>
     )
 }
 
--- Meals2Go/src/infrastructure/navigation/account_navigator.jsx
+++ Meals2Go/src/infrastructure/navigation/account_navigator.jsx
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 
 const AccountNavigator = () => {
     return (
-        <Stack.Navigator headerMode="none">
+        <Stack.Navigator screenOptions={{headerMode: "none"}}>
             <Stack.Screen name="Main" component={AccountScreen} />
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Register" component={RegisterScreen} />
