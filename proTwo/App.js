dcd7bf3e2bd0bfb72e1a98a60fee402d12bb4b16
DHughes666 <graziemolto6@gmail.com>
2024-01-11 13:50:27 +0100

    Styling successfull... Cool

--- proTwo/App.js
+++ proTwo/App.js
@@ -73,6 +73,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
+    backgroundColor: '#a065ec',
   }, 
   inputContainer: {
     flex: 1,
--- proTwo/components/goalInput_comp.jsx
+++ proTwo/components/goalInput_comp.jsx
@@ -35,10 +35,18 @@ const GoalInput = ({addGoal, visible, closeModal}) => {
                 />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
-                        <Button title='Add Goal' onPress={addGoalHandler}/>
+                        <Button 
+                            title='Add Goal' 
+                            onPress={addGoalHandler}
+                            color="#530acc"
+                        />
                     </View>
                     <View style={styles.button}>
-                        <Button title='Cancel' onPress={closeModal}/>
+                        <Button 
+                            title='Cancel' 
+                            onPress={closeModal}
+                            color="#f31282"
+                        />
                     </View>
                 </View>
             </View>
@@ -59,9 +67,12 @@ const styles = StyleSheet.create({
     },
     textInput: {
         borderWidth: 1, 
-        borderColor: '#cccccc',
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        color: '#120438',
+        borderRadius: 6,
         width: '100%',
-        padding: 8
+        padding: 16
     },
     buttonContainer: {
         flexDirection: 'row',
