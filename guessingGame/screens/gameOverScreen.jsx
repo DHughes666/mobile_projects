df4ed988ee48efe000f20915fe3ad01d77f9aece
DHughes666 <graziemolto6@gmail.com>
2024-01-17 19:06:52 +0100

    Tushing the 'GameOver' screen

--- guessingGame/screens/gameOverScreen.jsx
+++ guessingGame/screens/gameOverScreen.jsx
@@ -1,13 +1,46 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Image, Text, StyleSheet } from "react-native";
+import Title from "../components/ui/title";
+import Colors from "../constants/color";
 
 
 const GameOverScreen = () => {
     return (
-        <View>
-            <Text>Game Over!!!</Text>
+        <View style={styles.container}>
+            <Title>GAME OVER!</Title>
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={require('../assets/images/success.png')}
+                    style={styles.image}
+                />
+            </View>
+            <Text>
+                Your phone needed <Text>X</Text> rounds to guess the number Y.
+            </Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 200,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    }
+})
 
 export default GameOverScreen;
