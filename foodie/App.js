5b8e5e416e11e2762fb3f8c31386d536aba6ec7b
DHughes666 <graziemolto6@gmail.com>
2024-01-21 23:41:39 +0100

    Nesting navigators ...So far so cool

--- foodie/App.js
+++ foodie/App.js
@@ -2,12 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CategoriesScreen from './src/screens/categoriesScreen';
 import MealsOverViewScreen from './src/screens/mealsOverviewScreen';
 import MealDetailScreen from './src/screens/mealDetailScreen';
+import FavouriteScreen from './src/screens/favouriteScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Categories" component={CategoriesScreen}/>
+      <Drawer.Screen name="Favorites" component={FavouriteScreen} />
+    </Drawer.Navigator>
+  )
+}
 
 export default function App() {
   return (
@@ -22,9 +34,9 @@ export default function App() {
           }}
         >
           <Stack.Screen 
-            name='Meals Categories' 
-            component={CategoriesScreen}
-            options={{ title: 'Categories' }}
+            name='Drawer' 
+            component={DrawerNavigator}
+            // options={{ title: 'Categories' }}
           />
           <Stack.Screen 
             name='Meals Overview'
