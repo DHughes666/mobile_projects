99ba8430a3242b761beb123644fb3de99577ebcb
DHughes666 <graziemolto6@gmail.com>
2024-02-07 09:46:00 +0100

    Displaying favorites ...Successful

--- foodie/src/screens/favouriteScreen.jsx
+++ foodie/src/screens/favouriteScreen.jsx
@@ -1,5 +1,16 @@
-import { View, Text } from "react-native";
+import { useContext } from "react";
 
-const FavouriteScreen = ()  => {};
+import MealsList from "../components/mealsList/mealsList_comp";
+import { MEALS } from "../../data/dummy_data";
+import { FavouritesContext } from "../../store/context/favourite_context";
+
+const FavouriteScreen = ()  => {
+    const favouriteMealsContext = useContext(FavouritesContext)
+
+    const favouriteMeals = MEALS.filter(
+        (meal) => favouriteMealsContext.ids.includes(meal.id))
+
+    return <MealsList items={favouriteMeals}/>;
+};
 
 export default FavouriteScreen;
