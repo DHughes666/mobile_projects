afc5813382c548f0813d6bbbe847f23508747940
DHughes666 <graziemolto6@gmail.com>
2024-02-07 09:52:01 +0100

    Context ends ...Redux begins ...All for favouriteMeals

--- foodie/src/screens/favouriteScreen.jsx
+++ foodie/src/screens/favouriteScreen.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 import MealsList from "../components/mealsList/mealsList_comp";
 import { MEALS } from "../../data/dummy_data";
@@ -10,7 +11,30 @@ const FavouriteScreen = ()  => {
     const favouriteMeals = MEALS.filter(
         (meal) => favouriteMealsContext.ids.includes(meal.id))
 
+    if (favouriteMeals.length === 0) {
+        return (
+            <View style={styles.rootContainer}>
+                <Text style={styles.text}>
+                    You have no favourite meals yet.
+                </Text>
+            </View>
+        )
+    }
+
     return <MealsList items={favouriteMeals}/>;
 };
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'black'
+    }
+});
 
 export default FavouriteScreen;
