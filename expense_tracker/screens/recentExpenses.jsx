bbc4f001574c0151871d799d9e36d1f4aac5023a
DHughes666 <graziemolto6@gmail.com>
2024-03-02 23:05:08 +0100

    FOODIE - LoadingOverlay feature...Implemented

--- expense_tracker/screens/recentExpenses.jsx
+++ expense_tracker/screens/recentExpenses.jsx
@@ -1,22 +1,28 @@
 import { useContext, useState, useEffect } from "react";
 
 import { ExpensesContext } from "../store/expenses_context";
-
+import LoadingOverlay from "../components/UI/loadingOverlay";
 import ExpensesOutput from "../components/expensesOutput/expensesOutput";
 import { getDateMinusDays } from "../util/date";
 import { getExpenses } from "../util/http";
 
 const RecentExpenses = () => {
     const {expenses, setExpenses} = useContext(ExpensesContext)
+    const [isFetching, setIsFetching] = useState(true)
     
     useEffect(() => {
         async function fetchExpenses() {
             const expenses = await getExpenses();
+            setIsFetching(false);
             setExpenses(expenses);
         }
 
         fetchExpenses();
     },[])
+
+    if (isFetching) {
+        return <LoadingOverlay />
+    }
 
     const recentExpenses = expenses.filter((recExp) => {
         const today = new Date();
