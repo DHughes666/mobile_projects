58bc7c9786aa6476ae39b17a45fae76d0156b618
DHughes666 <graziemolto6@gmail.com>
2024-03-06 23:29:27 +0100

    AUTHENTICATION Project ... Phase_II -  Error handling...In progress

--- authen/screens/LoginScreen.jsx
+++ authen/screens/LoginScreen.jsx
@@ -1,7 +1,30 @@
+import { useState } from 'react';
+import { Alert } from 'react-native'
 import AuthContent from '../components/Auth/AuthContent';
+import { login } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 const LoginScreen = () => {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const loginHandler = async ({email, password}) => {
+    setIsAuthenticating(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        'Authentication failed',
+        'Could not log you in. Please check your credentials and try again'
+        )
+    }
+    setIsAuthenticating(false);
+  };
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Loggin in..." />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler}/>;
 }
 
 export default LoginScreen;
--- authen/util/auth.js
+++ authen/util/auth.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyDa-w5vsmfgHWGiP3hXYfkO042sN85KjiI'
-
-const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
-
-export const createUser = async (email, password) => {
+const authenticate = async (mode, email, password) => {
+    const API_KEY = 'AIzaSyDa-w5vsmfgHWGiP3hXYfkO042sN85KjiI'
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+    
     const response = await axios.post(url, {
         email: email, 
         password: password,
         returnSecureToken: true
     });
+
+};
+
+export const createUser = async (email, password) => {
+    await authenticate('signUp', email, password);
+}
+
+export const login = async (email, password) => {
+    await authenticate('signInWithPassword', email, password);
 }
