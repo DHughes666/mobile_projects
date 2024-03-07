cc10d3edc2b0f50270dda6125f5ce6c67899d25c
DHughes666 <graziemolto6@gmail.com>
2024-03-07 07:55:34 +0100

    AUTHENTICATION Project ... Phase_V ...Protecting database

--- authen/screens/WelcomeScreen.jsx
+++ authen/screens/WelcomeScreen.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState, useContext } from 'react';
+import axios from 'axios';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth_context';
 
 const WelcomeScreen = () => {
+  const [fetchedMessage, setFetchedMessage] = useState('')
+
+  const {token} = useContext(AuthContext)
+
+  useEffect(() => {
+    axios.get(
+      'https://foodie-418d2-default-rtdb.firebaseio.com/message.json?auth=' +
+        token
+    ).then((response) =>{
+      setFetchedMessage(response.data);
+    });
+  }, []);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
