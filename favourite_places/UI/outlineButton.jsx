42aa13511f605258efa9f3757e9e695f858b5861
DHughes666 <graziemolto6@gmail.com>
2024-03-09 22:03:15 +0100

    FAVOURITE PLACES - Working with images - Camera feature ...Implemented

---
+++ favourite_places/UI/outlineButton.jsx
@@ -0,0 +1,44 @@
+import { Pressable, Text, StyleSheet } from "react-native"
+import {Ionicons} from "@expo/vector-icons"
+
+import { Colors } from "../constants/colors"
+
+const OutlineButton = ({children, pressit, icon}) => {
+    return (
+        <Pressable 
+            style={({pressed}) => [styles.button, pressed && styles.pressed]}>
+            <Ionicons 
+                style={styles.icon}
+                name={icon}
+                size={18}
+                color={Colors.primary500}
+                onPress={pressit}
+            />
+            <Text style={styles.text}>{children}</Text>
+        </Pressable>
+    )
+};
+
+const styles = StyleSheet.create({
+    button: {
+        paddingHorizontal: 12,
+        paddingVertical: 6,
+        margin: 4,
+        flexDirection: "row",
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: Colors.primary500
+    },
+    pressed: {
+        opacity: 0.7
+    },
+    icon: {
+        marginRight: 6
+    },
+    text: {
+        color: Colors.primary500
+    },
+});
+
+export default OutlineButton;
--- favourite_places/components/places/imagePicker.jsx
+++ favourite_places/components/places/imagePicker.jsx
@@ -1,22 +1,89 @@
-import { View, Button } from "react-native";
-import {launchCameraAsync} from "expo-image-picker"
+import { useState } from "react";
+import { View, Text, Button, Alert, Image, StyleSheet } from "react-native";
+import {launchCameraAsync, 
+    useCameraPermissions, PermissionStatus} from "expo-image-picker"
+
+import {Colors} from '../../constants/colors';
+import OutlineButton from "../../UI/outlineButton";
 
 const ImagePicker = () => {
+    const [
+        cameraPermissionsInformation, 
+        requestPermission] = useCameraPermissions()
+    const [pickedImage, setPickedImage] = useState();
+
+    const verifyPermission = async () => {
+        if (cameraPermissionsInformation.status === PermissionStatus.UNDETERMINED) {
+            const permissionResponse = await requestPermission();
+
+            return permissionResponse.granted;
+        }
+
+        if (cameraPermissionsInformation.status === PermissionStatus.DENIED) {
+            Alert.alert(
+                "Insufficient Permission!",
+                "Camera permission required to use this app"
+            )
+
+            return false;
+        }
+
+        return true;
+    }
+
     const takeImageHandler = async () => {
+        const hasPermission = await verifyPermission();
+
+        if (!hasPermission) {
+            return;
+        }
+
         const image = await launchCameraAsync({
             allowsEditing: true,
             aspectRatio: [16, 9],
             quality: 0.5,
         });
-        console.log(image);
+        setPickedImage(image.assets[0].uri);
+        // console.log(image.assets[0]["uri"]);
+    }
+
+    let imagePreview = <Text>No image taken yet.</Text>
+
+    if (pickedImage) {
+        imagePreview = <Image style={styles.image}
+            source={{ uri: pickedImage }}
+        />
     }
 
     return (
         <View>
-            <View></View>
-            <Button title="Take Image" onPress={takeImageHandler} />
+            <View style={styles.imagePreview}>
+                {imagePreview}
+            </View>
+            <OutlineButton
+                icon="camera"
+                pressit={takeImageHandler}
+            >
+                Take Image
+            </OutlineButton>
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primary100,
+        borderRadius: 4
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+});
 
 export default ImagePicker;
